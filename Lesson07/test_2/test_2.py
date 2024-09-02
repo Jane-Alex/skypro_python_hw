b8dbe9ea341aa4aec0d06ad6605877fee0dc95c1
Jane-Alex <ryadnova@gmail.com>
2024-09-02 07:47:37 +0300

    Lesson07 deleted

--- Lesson07/test_2/test_2.py
+++
@@ -1,9 +0,0 @@
-from Lesson7.data import *
-from Lesson7.test_2.CalcPage import Calculator
-
-def test_check_calculator(driver):
-    calculator = Calculator(driver)
-    calculator.set_time()
-    calculator.click_buttons()
-
-    assert "15" in calculator.wait_result()
